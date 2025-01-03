0f68c1bd39b1bdfff94fa54e2919da3a2d0e068a
BakangMonei <bakangmonei2@gmail.com>
2025-01-03 20:00:00 +0200

    Okayyy

--- src/constants/General.js
+++ src/constants/General.js
@@ -1,22 +1,22 @@
 const WELCOME_CONTENTS = [
   {
-    image: 'DISCOVER',
-    title: 'Discover places near you',
+    image: "DISCOVER",
+    title: "Discover places near you",
     content:
-      'We make it simple to find your favorite food. Enter your address and let us do the rest.',
+      "We make it simple to find your favorite food. Enter your address and let us do the rest.",
   },
   {
-    image: 'FAVORITE',
-    title: 'Order your favorite',
+    image: "FAVORITE",
+    title: "Order your favorite",
     content:
-      'We will store your favorite foods based on your search and orders.',
+      "We will store your favorite foods based on your search and orders.",
   },
   {
-    image: 'DELIVER',
-    title: 'Fastest Delivery',
+    image: "DELIVER",
+    title: "Fastest Delivery",
     content:
-      'We make food ordering fast, easy and free. No matter you paid online or cash.',
+      "We make food ordering fast, easy and free. No matter you paid online or cash.",
   },
 ];
 
-export default {WELCOME_CONTENTS};
+export default { WELCOME_CONTENTS };
