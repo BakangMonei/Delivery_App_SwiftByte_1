e25a5242a935db444e1d58e975e9f18eb5746aca
BakangMonei <bakangmonei2@gmail.com>
2025-01-12 20:00:00 +0200

    Okayyy

--- src/services/index.js
+++ src/services/index.js
@@ -1,10 +1,10 @@
-import StaticImageService from './StaticImageService';
-import AuthenicationService from './AuthenticationService';
-import StorageService from './StorageService';
-import RestaurantService from './RestaurantService';
-import CartService from './CartService';
-import FoodService from './FoodService';
-import BookmarkService from './BookmarkService';
+import StaticImageService from "./StaticImageService";
+import AuthenicationService from "./AuthenticationService";
+import StorageService from "./StorageService";
+import RestaurantService from "./RestaurantService";
+import CartService from "./CartService";
+import FoodService from "./FoodService";
+import BookmarkService from "./BookmarkService";
 
 export {
   StaticImageService,
