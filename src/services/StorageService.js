862aa35a7ded021ac9f1f36505684ebe06394b71
BakangMonei <bakangmonei2@gmail.com>
2025-01-13 20:00:00 +0200

    Okayyy

--- src/services/StorageService.js
+++ src/services/StorageService.js
@@ -1,19 +1,19 @@
-import AsyncSorage from '@react-native-async-storage/async-storage';
+import AsyncSorage from "@react-native-async-storage/async-storage";
 
 const setFirstTimeUse = () => {
-  return AsyncSorage.setItem('isFirstTimeUse', 'true');
+  return AsyncSorage.setItem("isFirstTimeUse", "true");
 };
 
 const getFirstTimeUse = () => {
-  return AsyncSorage.getItem('isFirstTimeUse');
+  return AsyncSorage.getItem("isFirstTimeUse");
 };
 
-const setToken = token => {
-  return AsyncSorage.setItem('token', token);
+const setToken = (token) => {
+  return AsyncSorage.setItem("token", token);
 };
 
 const getToken = () => {
-  return AsyncSorage.getItem('token');
+  return AsyncSorage.getItem("token");
 };
 
-export default {setFirstTimeUse, getFirstTimeUse, setToken, getToken};
+export default { setFirstTimeUse, getFirstTimeUse, setToken, getToken };
